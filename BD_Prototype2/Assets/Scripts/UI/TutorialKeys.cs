2cfe6b4dc410118ec949cd863b732d94efbe53d2
MokaVodka <hoangnhito@gmail.com>
2024-07-02 16:14:06 +0200

    tutorial keys for move

---
+++ BD_Prototype2/Assets/Scripts/UI/TutorialKeys.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using BulletDance.Animation;
+
+public class TutorialKeys : RhythmAnimator
+{
+    private enum TutorialGuide { Move, Attack, Dash };
+
+    [SerializeField]
+    private TutorialGuide _guide;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        EventManager.Instance.OnKeyBoardAndMouseUsed += ChangeTutorialImage;
+        EventManager.Instance.OnGamePadUsed += ChangeTutorialImage;
+
+        _animationName = _guide.ToString();
+        _animator.SetFloat("Input", 0f);
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        EventManager.Instance.OnKeyBoardAndMouseUsed -= ChangeTutorialImage;
+        EventManager.Instance.OnGamePadUsed -= ChangeTutorialImage;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        EventManager.Instance.OnKeyBoardAndMouseUsed -= ChangeTutorialImage;
+        EventManager.Instance.OnGamePadUsed -= ChangeTutorialImage;
+    }
+
+
+    private void ChangeTutorialImage()
+    {
+        _animator.SetFloat("Input", 
+            InputManager.Instance.CurrentController == ControllerType.KEYBOARDANDMOUSE ?
+                0f : 1f );
+    }
+}
