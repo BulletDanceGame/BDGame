c78db4887d4b9e90e0ce7fed2ea2d5fd48c5284b
Rasmus Löfgren <rasmus.larsson-lofgren.7342@student.uu.se>
2024-07-13 02:38:29 +0200

    hmm

--- BD_Prototype2/Assets/GhostMovelist.cs
+++ BD_Prototype2/Assets/GhostMovelist.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -29,10 +30,12 @@ public class GhostMovelist : Movelist
 
     private bool isMoving;
     private Vector2 goalPosition;
+    private Vector2 lastPosition;
 
     [SerializeField] private Vector2 currentPosIndex;
     private Vector2 currentDirection;
-    bool clockwise;
+    private Vector2 lastPosIndex;
+    bool fromMiddle;
 
 
     private void OnEnable()
@@ -63,13 +66,17 @@ public class GhostMovelist : Movelist
 
     private void Shoot()
     {
-        if (currentPosIndex.x == 1 || currentPosIndex.y == 1)
+        if (currentPosIndex == Vector2.one)
+        {
+            MiddleShot();
+        }
+        else if (currentPosIndex.x == 1 || currentPosIndex.y == 1)
         {
             LineShot();
         }
         else
         {
-            ArrowShot();
+            CornerShot();
         }
 
         GetComponent<BoxCollider2D>().enabled = true;
@@ -84,10 +91,27 @@ public class GhostMovelist : Movelist
         Shooting.ShootInDirection(line, bulletPrefabs);
     }
 
-    private void ArrowShot()
+    private void CornerShot()
     {
-
         Vector2 dir = -transform.position; //towards 0,0
+        ArrowShot(dir);
+        
+        //backwards
+        //dir = transform.position;
+        //ArrowShot(dir);
+    }
+
+    private void MiddleShot()
+    {
+        ArrowShot(new Vector2(1, 1));
+        ArrowShot(new Vector2(1, -1));
+        ArrowShot(new Vector2(-1, 1));
+        ArrowShot(new Vector2(-1, -1));
+    }
+
+
+    private void ArrowShot(Vector2 dir)
+    {
         directionManager.up = dir;
 
         Shooting.ShootInDirection(arrow, bulletPrefabs);
@@ -110,82 +134,114 @@ public class GhostMovelist : Movelist
 
     }
 
-    private void MoveClockwise()
+
+    private void GetNewDir(bool clockwise)
     {
-        if (currentPosIndex.x == 1 || currentPosIndex.y == 1)
+        if (fromMiddle)
         {
-            //same direction or switch if switching clockwise
-
-            if (clockwise == false)
-            {
-                currentDirection *= -1;
-            }
-        }
-        else
-        {
-            if (currentPosIndex == new Vector2(0, 0)) //Top Left Corner
-            {
-                currentDirection = new Vector2(1, 0); //right
-            }
-            else if (currentPosIndex == new Vector2(2, 0)) //Top Right Corner
-            {
-                currentDirection = new Vector2(0, 1); //down
-            }
-            else if(currentPosIndex == new Vector2(2, 2)) //Bottom Right Corner
-            {
-                currentDirection = new Vector2(-1, 0); //left
-            }
-            else if(currentPosIndex == new Vector2(0, 2)) //Bottom Left Corner
-            {
-                currentDirection = new Vector2(0, -1); //up
-            }
+            currentPosIndex = lastPosIndex;
         }
 
+
+        if (clockwise)
+        {
+            if (currentPosIndex == new Vector2(0, 0) || currentPosIndex == new Vector2(1, 0)) //Top Left Corner
+            {
+                currentDirection = Vector2.right;
+            }
+            else if (currentPosIndex == new Vector2(2, 0) || currentPosIndex == new Vector2(2, 1) ) //Top Right Corner
+            {
+                currentDirection = Vector2.up;
+            }
+            else if (currentPosIndex == new Vector2(2, 2) || currentPosIndex == new Vector2(1, 2)) //Bottom Right Corner
+            {
+                currentDirection = Vector2.left;
+            }
+            else if (currentPosIndex == new Vector2(0, 2) || currentPosIndex == new Vector2(0, 1)) //Bottom Left Corner
+            {
+                currentDirection = Vector2.down;
+            }
+            else
+            {
+                Debug.Break();
+            }
+        }
+        else //counter
+        {
+            if (currentPosIndex == new Vector2(0, 0) || currentPosIndex == new Vector2(0, 1)) //Top Left Corner
+            {
+                currentDirection = Vector2.up;
+            }
+            else if (currentPosIndex == new Vector2(2, 0) || currentPosIndex == new Vector2(1, 0)) //Top Right Corner
+            {
+                currentDirection = Vector2.left;
+            }
+            else if (currentPosIndex == new Vector2(2, 2) || currentPosIndex == new Vector2(2, 1)) //Bottom Right Corner
+            {
+                currentDirection = Vector2.down;
+            }
+            else if (currentPosIndex == new Vector2(0, 2) || currentPosIndex == new Vector2(1, 2)) //Bottom Left Corner
+            {
+                currentDirection = Vector2.right;
+            }
+            else
+            {
+                Debug.Break();
+            }
+        }
+
+        
+    }
+
+    private void Move()
+    {
         currentPosIndex += currentDirection;
 
         isMoving = true;
         goalPosition = positionMatrix[(int)currentPosIndex.x][(int)currentPosIndex.y];
+        lastPosition = transform.position;
 
-        clockwise = true;
+        if (fromMiddle)
+        {
+            currentDirection = (currentPosIndex - Vector2.one).normalized;
+            fromMiddle = false;
+        }
+
+    }
+
+
+    private void MoveClockwise()
+    {
+        GetNewDir(true);
+
+
+        Move();
+
     }
 
     private void MoveCounterClockwise()
     {
-        if (currentPosIndex.x == 1 || currentPosIndex.y == 1)
-        {
-            //same direction or switch if switching clockwise
+        GetNewDir(false);
 
-            if (clockwise == true)
-            {
-                currentDirection *= -1;
-            }
-        }
-        else
-        {
-            if (currentPosIndex == new Vector2(0, 0)) //Top Left Corner
-            {
-                currentDirection = new Vector2(0, -1); //down
-            }
-            else if (currentPosIndex == new Vector2(2, 0)) //Top Right Corner
-            {
-                currentDirection = new Vector2(-1, 0); //left
-            }
-            else if (currentPosIndex == new Vector2(2, 2)) //Bottom Right Corner
-            {
-                currentDirection = new Vector2(0, -1); //up
-            }
-            else if (currentPosIndex == new Vector2(0, 2)) //Bottom Left Corner
-            {
-                currentDirection = new Vector2(1, 0); //right
-            }
-        }
+        Move();
 
-        currentPosIndex += currentDirection;
+    }
+
+    
+
+    private void MoveMiddle()
+    {
+        lastPosIndex = currentPosIndex;
+
+        currentPosIndex = Vector2.one;
+
+        currentDirection = (Vector2.one - lastPosIndex).normalized;
 
         isMoving = true;
         goalPosition = positionMatrix[(int)currentPosIndex.x][(int)currentPosIndex.y];
+        lastPosition = transform.position;
 
-        clockwise = false;
+        fromMiddle = true;
     }
 
 
@@ -193,12 +249,19 @@ public class GhostMovelist : Movelist
 
     private void Update()
     {
+        Moving();
+    }
+
+    private void Moving()
+    {
         if (isMoving)
         {
             float speed = (14f / (float)MusicManager.Instance.secondsPerBeat) * Time.deltaTime;
-            transform.position += new Vector3(currentDirection.x, currentDirection.y * -1, 0) * speed;
+            Vector3 dir = new Vector3(currentDirection.x, currentDirection.y * -1, 0);
+            transform.position += dir * speed;
 
-            if (Vector3.Distance(transform.position, goalPosition) <= 0.5f)
+            
+            if (Vector2.Distance(transform.position, lastPosition) >= Vector2.Distance(goalPosition, lastPosition))
             {
                 transform.position = goalPosition;
                 isMoving = false;
