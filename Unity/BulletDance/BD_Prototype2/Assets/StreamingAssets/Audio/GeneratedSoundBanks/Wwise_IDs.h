d4138bc7b182e7106baadaa48017cdc307394ea0
LupusCoffee <96193514+LupusCoffee@users.noreply.github.com>
2024-07-20 17:26:15 +0200

    testing stuff

--- Unity/BulletDance/BD_Prototype2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity/BulletDance/BD_Prototype2/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -193,6 +193,7 @@ namespace AK
         static const AkUniqueID PLAY_NAVCONFIRM = 2952062683U;
         static const AkUniqueID PLAY_NAVUPDOWN = 3540793454U;
         static const AkUniqueID PLAY_PAUSE_MUSIC = 1496939652U;
+        static const AkUniqueID PLAY_PICKUPSFX = 3043903975U;
         static const AkUniqueID PLAY_STARTGAME = 1444205428U;
         static const AkUniqueID PLAYER__AREYOUKIDDINGME____PLAY = 4096913669U;
         static const AkUniqueID PLAYER__BRINGIT____PLAY = 4003539485U;
