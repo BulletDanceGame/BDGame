5a07bf3237ce3536765d7415940d8e05e9152de9
Rasmus Löfgren <rasmus.larsson-lofgren.7342@student.uu.se>
2024-07-21 22:01:50 +0200

    calibration

--- BD_Prototype2/Assets/Scripts/Audio/Calibration/CalibrationMenu.cs
+++ BD_Prototype2/Assets/Scripts/Audio/Calibration/CalibrationMenu.cs
@@ -1,15 +1,49 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CalibrationMenu : MonoBehaviour
 {
     [SerializeField] private GameObject[] _screens;
+    [SerializeField] private TextMeshProUGUI[] _buttons;
     private int _currentScreen;
 
     [SerializeField] private MainMenu menu;
 
     public bool canHit { get; set; } = true;
 
+
+
+
+    public void ActivateScreen(int i)
+    {
+        _screens[_currentScreen].SetActive(false);
+        _buttons[_currentScreen].color = new Color(1f,1f,1f,0.5f);
+        _currentScreen = i;
+        _screens[_currentScreen].SetActive(true);
+        _buttons[_currentScreen].color = Color.white;
+
+        //if (MainMenu.currentController == ControllerType.GAMEPAD)
+        //{
+        //    MainMenu.currentButton = _screens[_currentScreen].GetComponentInChildren<Button>();
+        //    MainMenu.currentButton.Select();
+        //}
+    }
+
+    public void Done()
+    {
+
+        _screens[_currentScreen].SetActive(false);
+        _buttons[_currentScreen].color = new Color(1f, 1f, 1f, 0.5f);
+        _screens[0].SetActive(true);
+        _buttons[0].color = Color.white;
+        _currentScreen = 0;
+
+
+        SaveSystem.Instance.Save();
+
+        menu.BackFromCalibration();
+    }
 
     public void SwitchScreen(int i)
     {
