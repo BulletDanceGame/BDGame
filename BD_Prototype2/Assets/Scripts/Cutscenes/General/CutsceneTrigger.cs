0439d1e5e334f43a31930b6d3077ff8d297ffb4b
MokaVodka <hoangnhito@gmail.com>
2024-07-22 11:31:33 +0200

    miss cutscene

--- BD_Prototype2/Assets/Scripts/Cutscenes/General/CutsceneTrigger.cs
+++ BD_Prototype2/Assets/Scripts/Cutscenes/General/CutsceneTrigger.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class CutsceneTrigger : MonoBehaviour
 {
     [SerializeField]
+    private bool useTrigger = true, firstMiss = false;
+    [SerializeField]
     private string cutsceneName;
     private bool triggered = false;
+
+    void Start()
+    {
+        EventManager.Instance.OnPlayerMiss += StartMissCutsceen;
+    }
+
+    void OnDestroy()
+    {
+        EventManager.Instance.OnPlayerMiss -= StartMissCutsceen;
+    }
+
 
     void OnTriggerStay2D(Collider2D cld)
     {
         if(triggered) return;
+        if(!useTrigger) return;
 
         if(cld.tag == "Player")
-        {
-            EventManager.Instance?.StartCutscene(cutsceneName);
-            triggered = true;
-            HealPlayer();
-        }
+            StartCutsceen();
+    }
+
+    void StartCutsceen()
+    {
+        EventManager.Instance?.StartCutscene(cutsceneName);
+        triggered = true;
+        HealPlayer();
     }
 
     void HealPlayer()
     {
         UnitManager.Instance.GetPlayer().GetComponent<Player>().Heal(150);
     }
+
+
+    void StartMissCutsceen()
+    {
+        if(triggered) return;
+        if(!useTrigger && firstMiss)
+            StartCutsceen();
+    }
+
 }
