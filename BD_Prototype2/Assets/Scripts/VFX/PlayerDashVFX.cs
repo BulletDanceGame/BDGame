0a91ff1116ba4945383bf89b3b15122d8b15a7f6
MokaVodka <hoangnhito@gmail.com>
2024-10-14 11:44:13 +0200

    dash vfx fix

--- BD_Prototype2/Assets/Scripts/VFX/PlayerDashVFX.cs
+++ BD_Prototype2/Assets/Scripts/VFX/PlayerDashVFX.cs
@@ -20,11 +20,7 @@ public class PlayerDashVFX : MonoBehaviour
         EventManager.Instance.OnPlayerDash -= DashVFX;
     }
 
-
-    /* Main function */
-    [SerializeField] PlayerMovement _playerMovement;
-    [SerializeField] ParticleSystem _blink, _dust;
-    [SerializeField] int _dustAmount = 3;
+    /* Main function */ 
     void DashVFX(BeatTiming hitTiming, Vector2 direction)
     {
         //Blink
@@ -34,21 +30,52 @@ public class PlayerDashVFX : MonoBehaviour
         if(hitTiming == BeatTiming.BAD) return;
 
         //Dust
+        _dustOffsetAngle = Mathf.Sin(Vector2.Angle(direction, Vector2.up) * Mathf.Deg2Rad);
         for(int i = 0; i < _dustAmount; i++) {
-            Instantiate(_dust, _playerMovement.StartDashPosition, Quaternion.identity);
+            Instantiate(_dust, _playerMovement.StartDashPosition + _dustOffset * _dustOffsetAngle, Quaternion.identity);
         }
-
-        //Dash trail
-        EmitDashTrail(hitTiming);
 
         //After Image
         DashAfterImage(direction);
+
+        //Dash trail
+        EmitDashTrail(hitTiming);
+    }
+
+
+    [SerializeField] PlayerMovement _playerMovement;
+
+    /* Particles */
+    [Space] [Header("Particles")]
+    [SerializeField] ParticleSystem _blink;
+    [SerializeField] ParticleSystem _dust;
+    [SerializeField] int _dustAmount = 3;
+    [SerializeField] Vector2 _dustOffset = new Vector2(0, -1.3f);
+    float _dustOffsetAngle = 0f;
+
+    /* Dash AfterImage */
+    [Space] [Header("After Image")]
+    [SerializeField] SpriteRenderer _playerSpriteRenderer;
+    [SerializeField] Vector3 _afterImageOffset;
+    [SerializeField] private float _distanceBetweenImages;
+    [SerializeField] private Gradient _dashAfterImageColors;
+
+    void DashAfterImage(Vector2 direction)
+    {
+        for (int i = 0; i < _playerMovement.dashDistance; i+=2)
+        {
+            AfterImage img = AfterImagePool.Instance.GetFromPool(
+                transform, _playerSpriteRenderer, _afterImageOffset, 
+                _dashAfterImageColors.Evaluate(i/_playerMovement.dashDistance), true, true);
+            img.transform.position = (Vector3)_playerMovement.StartDashPosition + ((Vector3)direction * i) + _afterImageOffset;
+        }
     }
 
 
     /* Dash Trail */
 
-    [Space] [SerializeField]
+    [Space] [Header("Graffiti Trail")]
+    [SerializeField]
     private List<TrailRenderer> _trailRdr;
     private Color _baseColor;
 
@@ -61,10 +88,10 @@ public class PlayerDashVFX : MonoBehaviour
             _trailRdr[i].emitting = true;
 
             _trailRdr[i].material.SetColor("_BaseColor", _baseColor);
-            _trailRdr[i].material.SetFloat("_Hue", i * -0.6f + Random.Range(-0.07f, 0.07f));
-            _trailRdr[i].material.SetFloat("_Shape", Random.Range(0, 60));
+            _trailRdr[i].material.SetFloat("_Hue", i * -0.6f + Random.Range(-0.1f * (i+1), 0.1f * (i+1)));
+            _trailRdr[i].material.SetFloat("_Shape", Random.Range(5, 60));
             _trailRdr[i].material.SetFloat("_Intensity", Random.Range(7f, 30f));
-            _trailRdr[i].material.SetFloat("_Dispersity", Random.Range(-0.25f, 0.25f));
+            _trailRdr[i].material.SetFloat("_Dispersity", Random.Range(0.15f, 0.25f) * (Random.Range(0, 2) == 0 ? -1f : 1f));
         }
 
         _isEmitting = true;
@@ -97,24 +124,6 @@ public class PlayerDashVFX : MonoBehaviour
         }
     }
 
-
-    /* Dash AfterImage */
-    [Space]
-    [SerializeField] SpriteRenderer _playerSpriteRenderer;
-    [SerializeField] Vector3 _afterImageOffset;
-    [SerializeField] private float _distanceBetweenImages;
-    [SerializeField] private Gradient _dashAfterImageColors;
-
-    void DashAfterImage(Vector2 direction)
-    {
-        for (int i = 0; i < _playerMovement.dashDistance; i+=2)
-        {
-            AfterImage img = AfterImagePool.Instance.GetFromPool(
-                transform, _playerSpriteRenderer, _afterImageOffset, 
-                _dashAfterImageColors.Evaluate(i/_playerMovement.dashDistance), true, true);
-            img.transform.position = (Vector3)_playerMovement.StartDashPosition + ((Vector3)direction * i) + _afterImageOffset;
-        }
-    }
 }
 
 }
